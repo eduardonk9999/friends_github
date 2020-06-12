f84d976e47a74f87bcc4f94eab0571c96da01a8d
Eduardo <thiago.novak7@gmail.com>
2020-06-11 22:29:43 -0300

    Add, css, ajustes js

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 //user
 let campoUser = document.querySelector('.campo-user');
 let btnBuscarUser = document.querySelector('#btn-user');
+let imgHtml = document.querySelector('.user-img');
+let img = document.createElement('img');
+let ul = document.querySelector('.lista-repositorios');
 
 function BuscaUser() {
+    imgHtml.innerHTML = '';
   
   let campoValorUser = campoUser.value;
   axios.get(`https://api.github.com/users/${campoValorUser}`)
   .then(function(response){
-
+    let thumb = response.data.avatar_url;
     
     
-
-    var thumb = response.data.avatar_url;
-    var imgHtml = document.querySelector('.user-img');
     
-    var img = document.createElement('img');
     img.setAttribute('src', thumb);
 
     imgHtml.appendChild(img);
@@ -33,6 +33,8 @@ btnBuscarUser.onclick = BuscaUser;
 
 
 function userRepositorios() {
+  ul.innerHTML = '';
+
 //repositorios
 let campoValorUser = campoUser.value;
 axios.get(`https://api.github.com/users/${campoValorUser}/repos`)
@@ -42,7 +44,7 @@ axios.get(`https://api.github.com/users/${campoValorUser}/repos`)
 
     var listaRepos = response.data;
     listaRepos.forEach(function(item, indice){
-      let ul = document.querySelector('.lista-repositorios');
+      
       let li = document.createElement('li');
       let conteudo = document.createTextNode(item.name);
 
